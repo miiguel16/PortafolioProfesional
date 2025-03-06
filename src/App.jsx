f4b7c5cf95cbbcd2b075f8ef886a7266c6a3bf9f
Sena <dimatesebas73@gmail.com>
2025-03-06 15:37:33 -0500

    actualization a some things

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
     <>
       <Header/>
     <Routes> 
-      <Route path="/" content={<Home />}/>
+      <Route path="/" elementÑ={<Home />}/>
       <Route path="/SocialSkills" element ={< SocialSkills />} />
       <Route path="/ProfessionalExperience" element ={< ProfessionalExperience />} />
       <Route path="/TechnicalSkills" element ={< TechnicalSkills />} />
--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { ItemNav } from "../../components/ItemNav";
+import { ItemNav } from "../../Components/ItemNav";
 
 export const Navbar = ( ) => {
     return (
@@ -15,7 +15,7 @@ export const Navbar = ( ) => {
                     linksNav="clase 1"
                     myStyle='pr-4 text-zinc-50'
                     contentItem="Habilidades tecnicas"
-                    routes="/ProfessionalSkills"
+                    routes="/TechnicalSkills"
                 />
                 <ItemNav
                     linksNav="clase 2"
@@ -27,7 +27,7 @@ export const Navbar = ( ) => {
                     linksNav="clase 3"
                     myStyle='pr-4 text-zinc-50'
                     contentItem="Experiencia Profesional"
-                    routes="/TechnicalSkills"
+                    routes="/ProfessionalExperience"
                 />
                 <ItemNav
                     linksNav="clase 3"
