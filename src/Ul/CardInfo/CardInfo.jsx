534d2b37a425f686dd01c0c25be9158a827b1ae4
miiguel16 <miguelngelchilito@gmail.com>
2025-03-11 01:04:58 -0500

    actualization the data of the Technical Skills

--- src/Ul/CardInfo/CardInfo.jsx
+++ src/Ul/CardInfo/CardInfo.jsx
@@ -1,4 +1,4 @@
-export const CardInfo = ({ title, content,images }) => {
+export const CardInfo = ({ title, images, content }) => {
     return (
         <div className="w-11/12 sm:w-64 md:w-72 lg:w-96 xl:w-[400px] rounded-lg overflow-hidden shadow-lg bg-white border border-gray-200 transition-all duration-300 ease-in-out">
   
@@ -6,10 +6,6 @@ export const CardInfo = ({ title, content,images }) => {
             <h2 className="font-semibold text-xl text-gray-800 truncate flex justify-center items-center ">{title}</h2>
           </div>
   
-          <div className="px-6 py-4">
-            <p className="text-gray-600 text-base">{content}</p>
-          </div>
-
           <div className=" flex justify-center">
             {images && images.map((img, index) => (
 
@@ -17,10 +13,14 @@ export const CardInfo = ({ title, content,images }) => {
                 key={index}
                 src={img}
                 alt= {`imagen ${index + 1}`}
-                className="w-60 h-70 rounded-lg"
+                className="w-70 h-75 rounded-lg"
                 />
             ))}
           </div>
+          <div className="px-6 py-4">
+            <p className="text-gray-600 text-base h-auto">{content}</p>
+          </div>
+
   
         </div>
     );
--- src/pages/TechnicalSkills/TechnicalSkills.jsx
+++ src/pages/TechnicalSkills/TechnicalSkills.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { CardInfo } from '../../Ul/CardInfo/CardInfo'
 import Photo1 from "../../assets/images/Photo1.jpg";
+import Github from "../../assets/images/Github.jpg";
+
 
 
 export function TechnicalSkills() {
@@ -9,31 +11,33 @@ export function TechnicalSkills() {
 
       {/* <main  className="flex-1 p-4"> */}
 
-    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 p-4 '>
+    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 p-4 flec  '>
       
         <CardInfo
-          title="Habilidades Tecnicas"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi."   
+          title="Frontend"
+          content="HTML: En estructuras semánticas y accesibles.
+          CSS y Tailwind: Diseño adaptativo y responsive usando Flexbox, Grid y estilos utilitarios.
+          JavaScript: Manejo de eventos, manipulación del DOM junto a sincronía, asincronía y promesas.
+          React: Creación de componentes reutilizables, manejo de estado y consumo de APIs."   
           images={[Photo1]}
         />
         <CardInfo
-          title="Habilidades Tecnicas"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi."
+          title="Control de Versiones"
+          content="Git: Control de cambios y gestión de ramas.
+          GitHub: Repositorios, pull requests y despliegue con GitHub Pages."
+          images={[Github]}
+        />
+        <CardInfo
+          title="Educación y Liderazgo"
+          content="Lógica de Programación: Aplicación de estructuras de datos, algoritmos y buenas prácticas para la resolución eficiente de problemas.
+          Pensamiento Computacional: Desarrollo de habilidades analíticas para descomponer problemas complejos en soluciones programables.
+          Trabajo en equipo y colaboración: Participación activa en proyectos grupales.
+"
           images={[Photo1]}
         />
         <CardInfo
-          title="Habilidades Tecnicas"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi."
-          images={[Photo1]}
-        />
-        <CardInfo
-          title="Habilidades Tecnicas"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi."
-          images={[Photo1]}
-        />
-        <CardInfo
-          title="Habilidades Tecnicas"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi."
+          title="Desarrollo Web"
+          content="Aplicaciones del lado del cliente."
           images={[Photo1]}
         />
     </div>
