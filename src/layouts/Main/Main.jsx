1240344b6cfa30f9f840f510be8f9067359f5fca
miiguel16 <miguelngelchilito@gmail.com>
2025-03-12 01:12:51 -0500

    actualization the data in the Home

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,12 +1,32 @@
 import Photo1 from "../../assets/images/Photo1.jpg";
 
-export const Main =()=>{
-    return(
+export const Main = () => {
+    return (
         <>
-            <main className="size-full flex flex-col items-center justify-center" >
-                <h5 className="text-lg text-white font-semibold pb-5">Hola, mi nombre es Miguel Ángel Chilito Herrera, actualmente estudio en el Sena Analisis y desarrollo de software (ASDO)</h5>
-                <img className="w-full max-w-xl h-auto object-cover" src={Photo1} alt="imagen 1"/>
+            <main className="size-full flex flex-col md:flex-row items-center justify-center p-6">
+                {/* Imagen a la izquierda */}
+                <div className="md:w-1/3 flex justify-center">
+                    <img
+                        className="w-32 sm:w-48 md:w-64 lg:w-80 h-auto object-cover rounded-lg shadow-lg"
+                        src={Photo1}
+                        alt="imagen 1"
+                    />
+                </div>
+
+                {/* Texto par poner a la derecha */}
+                <div className="md:w-2/4 text-center md:text-left text-white font-semibold p-5">
+                    <p className="text-base">
+                        Soy Miguel Ángel Chilito Herrera, estudiante de Análisis y Desarrollo de Software (ADSO) en el Servicio Nacional de Aprendizaje (SENA). Actualmente, estoy en busca de oportunidades para aplicar y ampliar mis conocimientos en programación. Me caracterizo por ser una persona capaz de aprender nuevas cosas de manera rapida y practico así como mi compromiso con el aprendizaje continuo y el crecimiento profesional, respaldado por mi formación en el SENA.
+                    </p>
+                    <p className="mt-4">
+                        Tengo experiencia en JavaScript utilizando React.js, complementado con el framework de estilos Tailwind CSS. Me destaco por mi capacidad para trabajar en equipo, mi atención al detalle y mi habilidad para resolver problemas a través de soluciones innovadoras en el ámbito tecnológico.
+                    </p>
+                    <p className="mt-4">
+                        Mi objetivo es especializarme tanto en el desarrollo back-end como en el front-end , poder llegar a ser uno de los mejores "full stack" y formar parte de un entorno laboral que me permita seguir aprendiendo y contribuir a proyectos significativos.
+                    </p>
+                </div>
             </main>
+
         </>
     )
 }
--- src/pages/SocialSkills/SocialSkills.jsx
+++ src/pages/SocialSkills/SocialSkills.jsx
@@ -4,23 +4,46 @@ import Photo1 from "../../assets/images/Photo1.jpg";
 
 export function SocialSkills() {
   return (
-    <main>
-      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 p-4 flec  '>
+      <main className="size-full flex flex-col md:flex-row items-center justify-center p-6">
+                {/* Imagen a la izquierda */}
+                <div className="md:w-1/3 flex justify-center">
+                    <img
+                        className="w-32 sm:w-48 md:w-64 lg:w-80 h-auto object-cover rounded-lg shadow-lg"
+                        src={Photo1}
+                        alt="imagen 1"
+                    />
+                </div>
+
+                {/* Texto par poner a la derecha */}
+                <div className="md:w-2/4 text-center md:text-left text-white font-semibold p-5">
+                    <p className="text-base"> 
+                    A lo largo de mi formación académica y experiencia en proyectos, he desarrollado diversas habilidades sociales que me permiten interactuar de manera efectiva en diferentes entornos:
+                    </p>
+                    
+                    <p className="mt-4">
+                    Comunicación efectiva: Expreso mis ideas con claridad y comprendo diferentes perspectivas, lo que facilita el trabajo en equipo y la resolución de problemas.
+                    </p>
+
+                    <p className="mt-4">
+                    Liderazgo: Tengo la capacidad de motivar y guiar a un equipo hacia objetivos comunes, fomentando un ambiente de colaboración y crecimiento.
+
+                    </p>
+                    <p className="mt-4">
+                    Honestidad: Actúo con transparencia y responsabilidad en cada tarea, priorizando la ética y el compromiso en mi trabajo.
+                    </p>
+
+                    <p className="mt-4">
+                    Respeto: Valoro las opiniones y el trabajo de los demás, manteniendo siempre una actitud empática y profesional.
+                    </p>
+
+                    <p className="mt-4">
+                    Trabajo en equipo: Me integro fácilmente en grupos de trabajo, aportando ideas y colaborando para alcanzar metas colectivas de manera eficiente.
+                    </p>
 
 
-        <img className="w-full max-w-xs md:max-w-md lg:max-w-lg h-auto object-cover" src={Photo1} alt="imagen 1" />
 
 
-        <CardInfo
-          title="Mis habilidades sociales son"
-          content={`Comunicación efectiva: Expresión clara y comprensión de ideas en distintos contextos.\n
-          Liderazgo: Capacidad de inspirar y guiar a equipos hacia objetivos comunes.\n
-          Honestidad: Transparencia y ética en el trabajo y en la toma de decisiones.\n
-          Respeto: Consideración y empatía en la colaboración con otros.\n
-          Trabajo en equipo: Cooperación efectiva para alcanzar metas colectivas.`}
-        />
-
-      </div>
+                </div>
     </main>
   )
 }
